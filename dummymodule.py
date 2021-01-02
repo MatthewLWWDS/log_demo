58687378726f229898026a86c42f6b238a33c174
MatthewLWWDS <MatthewL.WWDS@gmail.com>
2021-01-02 23:50:44 +0900

    dummymodule is created

---
+++ dummymodule.py
@@ -0,0 +1,61 @@
+"""
+A dummy module to chack the operation of logging
+"""
+
+import logging
+import logging.config
+
+import logconfig
+
+conf_for_log=logconfig.readconfig()
+logging.config.dictConfig(conf_for_log)
+logger=logging.getLogger('simpleExample')
+
+class O_NotString(Exception):
+    """
+    Original exception
+    """
+    pass
+
+
+def testfunc(a):
+    return a
+
+
+def dm(*arg):
+    try:
+        a=arg[0]
+    except IndexError as ex:
+        logger.warning({
+            'action': 'getarg',
+            'exception' : ex,
+            'massage' : 'guess that no arguments are set.',
+            "name": __name__
+        })
+        a="dummy"
+
+    if type(a)!=str:
+        logger.warning({
+            'a' : a,
+            'action': 'cheking type',
+            "name": __name__,
+            'massage' : 'a is not str. program is stopped.'
+            'type(a)' : type(a)
+        })
+        raise O_NotString("arg[0] should be str")
+
+    result=testfunc(a)
+    logger.debug({
+            'a' : a,
+            "name": __name__,
+        })
+
+    return result
+
+
+
+
+
+
+if __name__=="__main__":
+    print(dm(4))
